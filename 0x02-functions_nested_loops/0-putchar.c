8ccaee569908a446c1e8a3b124b3006ee57340b2
betty <fagbamilabunmi96@gmail.com>
2022-09-14 15:20:48 -0700

    putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * main - check Description
+ *
+ * Description: write a program that prints putchar
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char nm = "Daniel";
+
+	while (*nm)
+	{
+		_putchar(*nm);
+		nm++;
+		_putchar('\n');
+
+		return (0);
